c19f7354fc8d6d79ac0f09a14b542a47741d4aec
Eve Reichmann <ereichmann4@aol.com>
2020-06-03 18:43:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,10 @@ end
 
 def list_dwarves(array)
   dwarves = ["Dopey", "Grumpy", "Bashful"]
-  item = 1
+  when item < dwarves.length do 
   dwarves.each_with_index do |index, item|
+    item += 1 
     puts "#{item}. #{index}"
   end  
+end  
 end
