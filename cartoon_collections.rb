b1b002505f44f072d494059845ece514375b8371
Eve Reichmann <ereichmann4@aol.com>
2020-06-03 18:38:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,7 @@ def greet_characters(array)
   characters_array = ["Dopey", "Grumpy", "Bashful"]
   characters_array.each do |characters_array|
     puts "Hello #{characters_array}!"
-end
-
+  end
 end
 
 def list_dwarves(array)
