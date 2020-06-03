3f2a877d5f14bb52bc2a6e58601c7f63b8ae33ac
Eve Reichmann <ereichmann4@aol.com>
2020-06-03 18:31:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,13 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
+  # oppressed_workers = ["dopey", "sneezy", "happy", "angry", "doc", "lemonjello", "sleepy" ]
+oppressed_workers.each do |oppressed_worker|
+  puts "#{oppressed_worker.capitalize} wants to start a union!"
   
+  
+end
+
 end
 
 def list_dwarves(array)
